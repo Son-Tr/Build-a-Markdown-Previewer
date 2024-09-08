7acaad3dde2997b013692c763b7e8c0e980cb706
Son-Tr <sontran.kh1502@gmail.com>
2024-09-08 17:01:51 +0700

    add function to handle when user drag divide

--- src/App.js
+++ src/App.js
@@ -4,77 +4,100 @@ import './App.css';
 import Editor from './editor/Editor';
 import Previewer from './previewer/Previewer';
 import { faUpRightAndDownLeftFromCenter, faDownLeftAndUpRightToCenter } from '@fortawesome/free-solid-svg-icons'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
   const maximize = <FontAwesomeIcon icon={faUpRightAndDownLeftFromCenter} />
   const minimize = <FontAwesomeIcon icon={faDownLeftAndUpRightToCenter} />
 
   const [isMaximize, setIsMaximize] = useState(false)
-  const [width,setWidth] =useState("50%");
-  const [isResizing,setResizing]= useState(false)
+  const [width, setWidth] = useState("50%");
+  const [isResizing, setResizing] = useState(false)
 
 
   /* ------------------------- handle toggle maximize ------------------------- */
-  const hangToggleMaximize = (string) => {
-      if(string==="editor"){
-        if(!isMaximize){
-          document.querySelector(".previewer-part").style.display= "none";
-          document.querySelector(".divide").style.display= "none";
-          setIsMaximize(!isMaximize);
-          document.querySelector("#editor").style.height= "100vh";
-        }else{
-          document.querySelector(".previewer-part").style.display= "block";
-          document.querySelector(".divide").style.display= "block";
-          setIsMaximize(!isMaximize);
-          document.querySelector("#editor").style.height= "100%";
-        }
-      }else{
-        if(!isMaximize){
-          document.querySelector(".editor-part").style.display= "none";
-          document.querySelector(".divide").style.display= "none";
-          setIsMaximize(!isMaximize);
-        }else{
-          document.querySelector(".editor-part").style.display= "block";
-          document.querySelector(".divide").style.display= "block";
-          setIsMaximize(!isMaximize);
-        }
+  const handleToggleMaximize = (string) => {
+    if (string === "editor") {
+      if (!isMaximize) {
+        document.querySelector(".previewer-part").style.display = "none";
+        document.querySelector(".divide").style.display = "none";
+        setIsMaximize(!isMaximize);
+        document.querySelector("#editor").style.height = "100vh";
+      } else {
+        document.querySelector(".previewer-part").style.display = "block";
+        document.querySelector(".divide").style.display = "block";
+        setIsMaximize(!isMaximize);
+        document.querySelector("#editor").style.height = "100%";
       }
-   
+    } else {
+      if (!isMaximize) {
+        document.querySelector(".editor-part").style.display = "none";
+        document.querySelector(".divide").style.display = "none";
+        setIsMaximize(!isMaximize);
+      } else {
+        document.querySelector(".editor-part").style.display = "block";
+        document.querySelector(".divide").style.display = "block";
+        setIsMaximize(!isMaximize);
+      }
+    }
+
   }
 
   /* -------------------------------- function to handle when user drag divide-------------------------------- */
   // start drag divide
- const hadleOnMouseDown=()=>{
+  const handleOnMouseDown = () => {
     setResizing(true)
   }
 
+  const handleOnMouseMove = (e) => {
+    if (!isResizing) {
+      return //if user don't drag divide
+    }
+    const containerOffSetLeft = document.querySelector(".container").offsetLeft;
+    const newLeftWidth = e.clientX - containerOffSetLeft;
+
+    setWidth(`${newLeftWidth}px`)
+  }
 
   // finish drag divide
- const hadleOnMouseUp=()=>{
+  const handleOnMouseUp = () => {
     setResizing(false)
   }
+
+  useEffect(() => {
+    if (isResizing) {
+      document.addEventListener("mousemove", handleOnMouseMove);
+      document.addEventListener("mouseup", handleOnMouseUp);
+    } else {
+      document.removeEventListener("mousemove", handleOnMouseMove);
+      document.removeEventListener("mouseup", handleOnMouseUp);
+    }
+  
+    return () => {
+      document.removeEventListener("mousemove", handleOnMouseMove);
+      document.removeEventListener("mouseup", handleOnMouseUp);
+    };
+  }, [isResizing]);
 
   return (
     <div className='app'>
       <h1 className='title'>Mardown Previewer</h1>
       <p className='author'>by <a href="https://github.com/Son-Tr" target='_blank' rel="noopener noreferrer">Son-Tr</a></p>
-      <div className='container'>
-        <div className='editor-part'>
+      <div className='container' >
+        <div className='editor-part' style={{width: width}}>
           <div className="toolbar">
             <h2 >Editor</h2>
-            <span className="icon" onClick={()=>{hangToggleMaximize("editor")}}>
+            <span className="icon" onClick={() => { handleToggleMaximize("editor") }}>
               {isMaximize ? minimize : maximize}
             </span>
           </div>
           <Editor />
         </div>
-        <div className='divide'></div>
-        <div className='previewer-part' style={{
-        }}>
+        <div className='divide' onMouseDown={handleOnMouseDown}></div>
+        <div className='previewer-part' style={{width:`calc(100%- ${width}-8px)`}}>
           <div className="toolbar">
             <h2 >Previewer</h2>
-            <span className="icon" onClick={()=>{hangToggleMaximize("previewer")}}>
+            <span className="icon" onClick={() => { handleToggleMaximize("previewer") }}>
               {isMaximize ? minimize : maximize}
             </span>
           </div>
